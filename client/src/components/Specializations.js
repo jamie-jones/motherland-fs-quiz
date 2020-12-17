4c5a0a97909cf07e6657cd4a6ea939261eef4672
Insomnia <jamie.mar.jones14@gmail.com>
2020-12-16 21:58:40 -0500

    adding some new things for the results

--- client/src/components/Specializations.js
+++ client/src/components/Specializations.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from "prop-types";
 
 class Specializations extends Component {
-    render() {
+    renderFixer() {
         return (
             <div>
                 
